b4a8cef8e7df278ecbe6366d2d09f5afc4afb6cd
Sally Poundall <sally_business@hotmail.co.uk>
2013-07-30 18:18:38 +0100

    app

--- app.rb
+++ app.rb
@@ -3,3 +3,52 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+get '/:name/:name2/mrdarcy' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :mrdarcy
+end
+
+get '/:name/:name2/romeojuliet' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :romeojuliet
+end
+
+get '/:name/:name2/cummings' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :cummings
+end
+
+get '/:name/:name2/heaven' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :heaven
+end
+
+get '/:name/:name2/heaven/:number' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    @number = params[:number]
+    erb :heaven_number
+end
+
+get '/:name/:name2/insurance' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :insurance
+end
+
+get '/:name/:name2/ass' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :ass
+end
+
+get '/:name/:name2/polish' do
+    @name = params[:name]
+    @name2 = params[:name2]
+    erb :polish
+end
---
+++ views/romeojuliet.erb
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href='bootstrap/css/bootstrap.css' rel='stylesheet'>
+</head>
+<body>
+<p>Dearest <%= @name.capitalize %>,</p>
+<p>My bounty is as boundless as the sea,</p>
+<p>My love as deep; the more I give to thee,</p>
+<p>The more I have, for both are infinite.</p> 
+<p><%= @name2.capitalize %>
+</body>
+</html>
+
+
